7cb9670638be06ad992da5691ed447eb34ce6e52
chetan chandel <chetanchandel3@gmail.com>
2022-08-22 00:22:14 +0530

    feat: ques command

--- src/commands/ques.ts
+++ src/commands/ques.ts
@@ -1,23 +1,58 @@
-import { Arguments, CommandBuilder } from "yargs";
+import { exec, ExecException, ProcessEnvOptions } from "child_process";
+import path from "path";
+import { Arguments } from "yargs";
 
-type Options = {
-  name: string;
-  upper: boolean | undefined;
+/** promisified exec */
+type ExecCallback = (
+  error: ExecException | null,
+  stdout: string,
+  stderr: string
+) => void;
+
+type PromisifiedExecArgs = {
+  cmd: string;
+  options?: ProcessEnvOptions;
+  cb: ExecCallback;
 };
 
-export const command: string = "ques <name>";
-export const desc: string = "some other command";
-
-export const builder: CommandBuilder<Options, Options> = (yargs) =>
-  yargs
-    .options({
-      upper: { type: "boolean" },
+const promisifiedExec = ({ cb, cmd, options }: PromisifiedExecArgs) =>
+  new Promise((resolve) =>
+    exec(cmd, { ...options }, (error, stdout, stderr) => {
+      cb(error, stdout, stderr);
+      resolve(null);
     })
-    .positional("name", { type: "string", demandOption: true });
+  );
+/** promisified exec */
 
-export const handler = (argv: Arguments<Options>): void => {
-  const { name, upper } = argv;
-  const greeting = `Ques, ${name}!`;
-  process.stdout.write(upper ? greeting.toUpperCase() : greeting);
+export const command: string = "ques";
+export const desc: string = "open ques in vscode";
+
+export const handler = async (argv: Arguments) => {
+  if (typeof process.env.PATH_TO_QUES !== "string") {
+    return console.log("please first setup a path for `ques` directory");
+  }
+
+  process.stdout.write("opening ques dir ⏳\n");
+
+  // w/o promisifying, we'd reach end of program and run `process.exit(0)` before exec gets to execute the command
+  // so we just suspend further execution by promisifying it
+
+  await promisifiedExec({
+    cmd: "code .",
+    cb: (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+      if (stdout) console.log(`stdout: ${stdout}`);
+    },
+    options: { cwd: path.join(process.env.PATH_TO_QUES) },
+  });
+
+  process.stdout.write("done 🎉");
   process.exit(0);
 };
