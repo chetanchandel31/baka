43a715c8e9da48db22ee3d2e6eb12da79a3870d0
chetan chandel <chetanchandel3@gmail.com>
2022-08-21 12:53:18 +0530

    setup scripts and entry point

--- src/cli.ts
+++ src/cli.ts
@@ -8,6 +8,8 @@ import yargs from "yargs";
 yargs(process.argv.slice(2))
   // Use the commands directory to scaffold.
   .commandDir("commands")
+  // properly prefix commands with `baka` in --help menu
+  .scriptName("baka")
   // Enable strict mode.
   .strict()
   // Useful aliases.
